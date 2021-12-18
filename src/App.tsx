26b66202449286bb5db7909facedabd593129d39
Agywa <91954956+Agywa@users.noreply.github.com>
2021-12-18 17:00:16 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
-function sum(a: number, b: number) {
-
-    alert(a + b)
-}
-
-//sum (1,2)
 
 
 function App() {
@@ -16,20 +11,29 @@ function App() {
     return (<div>
 
             {/*<input/>*/}
-            {/*<input value={"yo"} type={"pasword"}/>*/}
+            {/*<input value={"yo"} type={"password"}/>*/}
 
             {/*<PageTitle title = {"This is App Component"}/>*/}
             {/*<PageTitle title = {"My friends"}/>*/}
             {/*Article 1*/}
             {/*<Rating value={3}/>*/}
-            <Accordion titleValue = {"Meeeei"} collapsed={true}/>
-            <Accordion titleValue = {"Uses"} collapsed = {false}/>
-            {/*Article 2*/}
-            {/*<Rating value={3}/>*/}
+            {/*<Accordion titleValue = {"Meeeei"} collapsed={true}/>*/}
+            {/*<Accordion titleValue = {"Uses"} collapsed = {false}/>*/}
+
+
+
             {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
             {/*<Rating value={2}/>*/}
-            {/*<Rating value={5}/>*/}
             {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+
+
+            <OnOff/>
+            <OnOff/>
+            <OnOff/>
+
         </div>
     );
 }
